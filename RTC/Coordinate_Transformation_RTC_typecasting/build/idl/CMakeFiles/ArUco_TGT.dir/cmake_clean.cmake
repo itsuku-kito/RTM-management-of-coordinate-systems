9c6c5410b58860e5c2960c75042cbd0bbcdc82d8
itsuku-kito <129820033+itsuku-kito@users.noreply.github.com>
2023-11-29 13:09:34 +0900

    Add files via upload

---
+++ RTC/Coordinate_Transformation_RTC_typecasting/build/idl/CMakeFiles/ArUco_TGT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "ArUcoSkel.cpp"
+  "ArUcoSkel.h"
+  "CMakeFiles/ArUco_TGT"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ArUco_TGT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RTC/Coordinate_Transformation_RTC_typecasting/build/src/CMakeFiles/Coordinate_transformation_RTC_type_casting.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Coordinate_transformation_RTC_type_casting.dir/Coordinate_transformation_RTC_type_casting.cpp.o"
+  "CMakeFiles/Coordinate_transformation_RTC_type_casting.dir/__/idl/ArUcoSkel.cpp.o"
+  "Coordinate_transformation_RTC_type_casting.pdb"
+  "Coordinate_transformation_RTC_type_casting.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Coordinate_transformation_RTC_type_casting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
