c7076df7a90c9b473a629e16f99066389b824c86
itsuku-kito <129820033+itsuku-kito@users.noreply.github.com>
2023-11-29 13:05:51 +0900

    Delete RTC/build directory

--- RTC/build/CMakeFiles/Coordinate_transformation_RTC_type_casting_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Coordinate_transformation_RTC_type_casting_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Coordinate_transformation_RTC_type_casting_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
