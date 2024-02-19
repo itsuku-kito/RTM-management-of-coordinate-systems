18a0117aee14fb228d6fe0c9b518837ee44f7296
itsuku-kito <129820033+itsuku-kito@users.noreply.github.com>
2024-02-19 11:47:07 +0900

    Add files via upload

--- RTC/Coordinate_transformation_RTC/Coordinate_transformation_RTC.py
+++ RTC/Coordinate_transformation_RTC/Coordinate_transformation_RTC.py
@@ -10,10 +10,13 @@
 
 
 """
+
+
 # </rtc-template>
 
 import sys
 import time
+import datetime
 
 import xml.etree.ElementTree as ET
 import numpy as np
@@ -26,17 +29,13 @@ sys.path.append(".")
 import RTC
 import OpenRTM_aist
 
+import arUco
 
 # Import Service implementation class
 # <rtc-template block="service_impl">
 
 # </rtc-template>
-
-# Import Service stub modules
-# <rtc-template block="consumer_import">
-# </rtc-template>
-
-# URDFファイルを指定
+ #URDFファイルを指定
 tree = ET.parse('open_manipulator.urdf')#読み込むURDFファイルの名前に変更
 root = tree.getroot()
 
@@ -54,8 +53,13 @@ R_tree = []
 R_joint = np.identity(4)
 R_link = np.identity(4)
 
+
 # 表示する深さを指定
 display_depth = 7  # 例として7としていますが、必要に応じて変更してください
+
+# Import Service stub modules
+# <rtc-template block="consumer_import">
+# </rtc-template>
 
 
 # This module's spesification
@@ -98,15 +102,22 @@ class Coordinate_transformation_RTC(OpenRTM_aist.DataFlowComponentBase):
     def __init__(self, manager):
         OpenRTM_aist.DataFlowComponentBase.__init__(self, manager)
 
-        self._d_pre_transformation_coordinates = OpenRTM_aist.instantiateDataType(RTC.TimedPoint3D)
+        self._d_pre_transformation_coordinates1 = OpenRTM_aist.instantiateDataType(RTC.TimedPoint3D)
         """
         """
-        self._pre_transformation_coordinatesIn = OpenRTM_aist.InPort("pre_transformation_coordinates", self._d_pre_transformation_coordinates)
-        self._d_transformed_coordinates = OpenRTM_aist.instantiateDataType(RTC.TimedFloatSeq)
+        self._pre_transformation_coordinates1In = OpenRTM_aist.InPort("pre_transformation_coordinates1", self._d_pre_transformation_coordinates1)
+        self._d_pre_transformation_coordinates2 = OpenRTM_aist.instantiateDataType(arUco.arUcoPose3D)
+        """
+        """
+        self._pre_transformation_coordinates2In = OpenRTM_aist.InPort("pre_transformation_coordinates2", self._d_pre_transformation_coordinates2)
+        self._d_transformed_coordinates = OpenRTM_aist.instantiateDataType(RTC.TimedPoint3D)
         """
         """
         self._transformed_coordinatesOut = OpenRTM_aist.OutPort("transformed_coordinates", self._d_transformed_coordinates)
+
+
 		
+
 
         # initialize of configuration-data.
         # <rtc-template block="init_conf_param">
@@ -125,6 +136,7 @@ class Coordinate_transformation_RTC(OpenRTM_aist.DataFlowComponentBase):
 		
         # </rtc-template>
 
+
 		 
     ##
     #
@@ -139,7 +151,8 @@ class Coordinate_transformation_RTC(OpenRTM_aist.DataFlowComponentBase):
         self.bindParameter("result_transformation_ID", self._result_transformation_ID, "1")
 		
         # Set InPort buffers
-        self.addInPort("pre_transformation_coordinates",self._pre_transformation_coordinatesIn)
+        self.addInPort("pre_transformation_coordinates1",self._pre_transformation_coordinates1In)
+        self.addInPort("pre_transformation_coordinates2",self._pre_transformation_coordinates2In)
 		
         # Set OutPort buffers
         self.addOutPort("transformed_coordinates",self._transformed_coordinatesOut)
@@ -200,7 +213,6 @@ class Coordinate_transformation_RTC(OpenRTM_aist.DataFlowComponentBase):
     #
     #
     def onActivated(self, ec_id):
-        print("activate")
     
         return RTC.RTC_OK
 	
@@ -228,17 +240,18 @@ class Coordinate_transformation_RTC(OpenRTM_aist.DataFlowComponentBase):
     #
     def onExecute(self, ec_id):
 
-                #ルート要素から探索を開始
-        if self._pre_transformation_coordinatesIn.isNew() :
+        #１
+        
+        if self._pre_transformation_coordinates1In.isNew() :
             print("Received Maker")
-            pose = self._pre_transformation_coordinatesIn.read()
+            pose = self._pre_transformation_coordinates1In.read()
             print(pose)
             
-            pose_X = pose.data.x
-            pose_Y = pose.data.y
-            pose_Z = pose.data.z
+            pose_X = pose.translates[0].x
+            pose_Y = pose.translates[0].y
+            pose_Z = pose.translates[0].z
 
-            poseIn = np.array([pose_X,pose_Y,pose_Z,1],dtype = "float")
+            poseIn = np.array([pose_X,pose_Y,pose_Z,1],dtype = "float")#入力データ
             print(poseIn)
             
             display_links_and_joints(root)
@@ -260,19 +273,48 @@ class Coordinate_transformation_RTC(OpenRTM_aist.DataFlowComponentBase):
 
             poseOut = np.dot(R_result,poseIn)
             print(poseOut)
-            self._d_transformed_coordinates.data = poseOut
-            
-            """
-            self._d_transformed_coordinates.data[0] = poseOut[0]
-            self._d_transformed_coordinates.data[1] = poseOut[1]
-            self._d_transformed_coordinates.data[2] = poseOut[2]
-            """
+            self._d_transformed_coordinates.data = poseOut#出力データ
 
             print(self._d_transformed_coordinates)
             self._transformed_coordinatesOut.write()
+
+        #2
+
+        if self._pre_transformation_coordinates2In.isNew() :
+            
+            pose = self._pre_transformation_coordinates2In.read()
             
             
+            pose_X = pose.translates[0].x
+            pose_Y = pose.translates[0].y
+            pose_Z = pose.translates[0].z
+
+            poseIn = np.array([pose_X,pose_Y,pose_Z,1],dtype = "float")#入力データ
             
+            display_links_and_joints(root)
+
+            tree_dict = check_matching_child_parent(joint_datas)
+
+            # 最上位の親ノードを取得
+            root_node = [node for node in tree_dict.values() if node.parent is None][0]
+
+            display_tree(root_node)
+
+            # 親ノードのデータを取得
+            parent_data = root_node.name
+
+            # 指定された深さまでの子ノードを表示
+            display_children(root_node, display_depth)
+
+            R_result = matrix_calculation(R_tree)
+
+            poseOut = np.dot(R_result,poseIn)
+            self._d_transformed_coordinates.data.x = poseOut[0]#出力データ
+            self._d_transformed_coordinates.data.y = poseOut[1]
+            self._d_transformed_coordinates.data.z = poseOut[2]
+
+            self._transformed_coordinatesOut.write() 
+    
         return RTC.RTC_OK
 	
     ###
@@ -340,44 +382,40 @@ class Coordinate_transformation_RTC(OpenRTM_aist.DataFlowComponentBase):
     #def onRateChanged(self, ec_id):
     #
     #    return RTC.RTC_OK
-
+	
 def display_links_and_joints(element, indentation=""):
     if element.tag == 'joint':
         display_joint_information(element)
     for child in element:
         display_links_and_joints(child, indentation + "  ")
 
-
 def display_joint_information(element):
         joint_name = element.attrib.get('name', 'N/A')
         rpy = None  # rpyを初期化
         if joint_name not in displayed_joint_names:
-            displayed_joint_names.add(joint_name)
+            displayed_joint_names.add(joint_name)#jointの名前取得
             parent_link = element.find('./parent')
             child_link = element.find('./child')
-            parent_name = parent_link.attrib.get('link', 'N/A')
-            child_name = child_link.attrib.get('link', 'N/A')
+            parent_name = parent_link.attrib.get('link', 'N/A')#親リンク取得
+            child_name = child_link.attrib.get('link', 'N/A')#子リンク取得
             if parent_link is not None and child_link is not None:
                 origin = element.find('./origin')
                 axis = element.find('./axis')
                 if origin is not None:
-                    joint_rpy_str = origin.get('rpy', 'N/A')
-                    joint_xyz_str = origin.get('xyz', 'N/A')
+                    joint_rpy_str = origin.get('rpy', 'N/A')#rpy取得
+                    joint_xyz_str = origin.get('xyz', 'N/A')#xyz取得
                     joint_rpy = [float(val) for val in joint_rpy_str.split()] if joint_rpy_str != 'N/A' else None
                     joint_xyz = [float(val) for val in joint_xyz_str.split()] if joint_xyz_str != 'N/A' else None
                     if axis is not None:
                        rpy_str = axis.get('xyz', 'N/A')
                        rpy = [float(val) for val in rpy_str.split()] if rpy_str != 'N/A' else None
-            rotation_matrix = compute_rotation_matrix(joint_rpy)
+            rotation_matrix = compute_rotation_matrix(joint_rpy)#回転行列取得
             R_joint[:3, :3] = rotation_matrix
             R_joint[:3, 3] = joint_xyz
-            #print(R_joint)
-            R_joints.append(np.copy(R_joint))
             joint_data = ([joint_name, parent_name, child_name, joint_rpy, joint_xyz, np.copy(R_joint)])
             joint_datas.append(joint_data)
         
         return True
-
 
 def compute_rotation_matrix(rpy):
     roll, pitch, yaw = rpy
@@ -389,7 +427,7 @@ def compute_rotation_matrix(rpy):
 
 def matrix_calculation(Rs) :
     result = Rs[0]
-    for i in range(self._reference_transformation_ID,self._result_transformation_ID = [1]):
+    for i in range(0,1):
         result = np.dot(result, Rs[i])#変換行列導出
     return result	
 
@@ -407,19 +445,17 @@ def check_matching_child_parent(joint_datas):
 
                     # 親ノードがまだ辞書に存在しない場合、新しくノードを作成
                     if parent_name not in tree_dict:
-                        tree_dict[parent_name] = Node(joint_datas[j][0])
+                        tree_dict[parent_name] = Node(joint_datas[j][5])
 
                     # 子ノードを親ノードの下に追加
                     if child_name not in tree_dict:
-                        tree_dict[child_name] = Node(joint_datas[i][0], parent=tree_dict[parent_name])
-
+                        tree_dict[child_name] = Node(joint_datas[i][5], parent=tree_dict[parent_name])
     return tree_dict
 
 # ツリーの表示
 def display_tree(root_node):
     for pre, fill, node in RenderTree(root_node):
         print(f"{pre}{node.name}")
-
 
 def display_children(node, depth, current_depth=0):
     if current_depth <= depth:
